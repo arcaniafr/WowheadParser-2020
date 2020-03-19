463eedd0558eda0287303ae28154df4a47e4a141
arcaniafr <56791020+arcaniafr@users.noreply.github.com>
2020-03-19 22:39:37 +0100

    init repo

---
+++ WowHeadParser/Entities/Spell.cs
@@ -0,0 +1,185 @@
+﻿/*
+ * * Created by Traesh for AshamaneProject (https://github.com/AshamaneProject)
+ */
+using Newtonsoft.Json;
+using Sql;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using static WowHeadParser.MainWindow;
+
+namespace WowHeadParser.Entities
+{
+    class SpellTemplateParsing
+    {
+        public int id;
+    }
+    
+    class SpellLootParsing
+    {
+        public int id;
+        public dynamic modes;
+        public int[] stack;
+        public int[] creates;
+
+        public string percent;
+        public string questRequired;
+        public string mode;
+    }
+
+    class SpellLootItemParsing : SpellLootParsing
+    {
+        public int classs;
+        public int count;
+        public int[] bonustrees;
+    }
+
+    class SpellLootCurrencyParsing : SpellLootParsing
+    {
+        public int category;
+        public string icon;
+    }
+    
+    class Spell : Entity
+    {
+        public Spell()
+        {
+            m_spellTemplateData = new SpellTemplateParsing();
+            m_spellTemplateData.id = 0;
+        }
+
+        public Spell(int id)
+        {
+            m_spellTemplateData = new SpellTemplateParsing();
+            m_spellTemplateData.id = id;
+        }
+
+        public override String GetWowheadUrl()
+        {
+            return GetWowheadBaseUrl() + "/spell=" + m_spellTemplateData.id;
+        }
+
+        public override List<Entity> GetIdsFromZone(String zoneId, String zoneHtml)
+        {
+            String pattern = @"new Listview\(\{template: 'npc', id: 'npcs', name: LANG\.tab_npcs, tabs: tabsRelated, parent: 'lkljbjkb574', note: WH\.sprintf\(LANG\.lvnote_filterresults, '\/npcs\?filter=cr=6;crs=" + zoneId + @";crv=0'\), data: (.+)\}\);";
+            String spellJSon = Tools.ExtractJsonFromWithPattern(zoneHtml, pattern);
+
+            List<SpellTemplateParsing> parsingArray = JsonConvert.DeserializeObject<List<SpellTemplateParsing>>(spellJSon);
+            List<Entity> tempArray = new List<Entity>();
+            foreach (SpellTemplateParsing spellTemplateStruct in parsingArray)
+            {
+                Spell spell = new Spell(spellTemplateStruct.id);
+                tempArray.Add(spell);
+            }
+
+            return tempArray;
+        }
+
+        public override bool ParseSingleJson(int id = 0)
+        {
+            if (m_spellTemplateData.id == 0 && id == 0)
+                return false;
+            else if (m_spellTemplateData.id == 0 && id != 0)
+                m_spellTemplateData.id = id;
+
+            String spellHtml = Tools.GetHtmlFromWowhead(GetWowheadUrl(), webClient);
+
+            if (spellHtml.Contains("inputbox-error") || spellHtml.Contains("database-detail-page-not-found-message"))
+            {
+                return false;
+            }
+            else
+            {
+                String dataPattern = @"\$\.extend\(g_spells\[" + m_spellTemplateData.id + @"\], (.+)\);";
+
+                String spellTemplateDataJSon = Tools.ExtractJsonFromWithPattern(spellHtml, dataPattern);
+                if (spellTemplateDataJSon != null)
+                {
+                    SpellTemplateParsing spellTemplateData = JsonConvert.DeserializeObject<SpellTemplateParsing>(spellTemplateDataJSon);
+
+                    SetSpellTemplateData(spellTemplateData);
+
+                    // Without m_spellTemplateData we can't really do anything, so return false
+                    if (m_spellTemplateData == null)
+                        return false;
+                }
+
+                if (IsCheckboxChecked("spell loot"))
+                {
+                    String spellRecipePattern = @"new Listview\({template: 'spell', id: 'recipes', name: LANG\.tab_recipes, tabs: tabsRelated, parent: 'lkljbjkb574', sort: \['skill', 'name'\], hiddenCols: \['slot'\], visibleCols: \['source'\], data:(.+)}\);";
+                    String spellRecipeJSon = Tools.ExtractJsonFromWithPattern(spellHtml, spellRecipePattern);
+                    if (spellRecipeJSon != null)
+                    {
+                        SpellLootItemParsing[] spellLootDatas = spellRecipeJSon != null ? JsonConvert.DeserializeObject<SpellLootItemParsing[]>(spellRecipeJSon) : new SpellLootItemParsing[0];
+                        SetSpellRecipeData(spellLootDatas);
+                    }
+                }
+
+
+                return true;
+            }
+        }
+
+        public void SetSpellTemplateData(SpellTemplateParsing spellData)
+        {
+            m_spellTemplateData = spellData;
+        }
+
+        public void SetSpellRecipeData(SpellLootItemParsing[] spellLootItemDatas)
+        {
+            List<SpellLootParsing> lootsData = new List<SpellLootParsing>();
+
+            SpellLootParsing[] allLootData = new SpellLootParsing[spellLootItemDatas.Length];
+            Array.Copy(spellLootItemDatas, allLootData, spellLootItemDatas.Length);
+
+            for (uint i = 0; i < allLootData.Length; ++i)
+            {
+                if (allLootData[i].creates[1] == 0)
+                    allLootData[i].creates[1] = 1;
+                if (allLootData[i].creates[2] == 0)
+                    allLootData[i].creates[2] = 1;
+                lootsData.Add(allLootData[i]);
+            }
+            m_spellLootDatas = lootsData.ToArray();
+        }
+
+        public override String GetSQLRequest()
+        {
+            String returnSql = "";
+
+            if (m_spellTemplateData.id == 0 || isError)
+                return returnSql;
+
+
+            if (IsCheckboxChecked("spell loot") && m_spellLootDatas != null)
+            {
+                m_spellLootBuilder = new SqlBuilder("spell_loot_template", "entry", SqlQueryType.DeleteInsert);
+                m_spellLootBuilder.SetFieldsNames("Item", "Reference", "Chance", "QuestRequired", "LootMode", "GroupId", "MinCount", "MaxCount", "Comment");
+
+                foreach (SpellLootParsing spellLootData in m_spellLootDatas)
+                {
+                    m_spellLootBuilder.AppendFieldsValue(m_spellTemplateData.id, // Entry
+                                                                spellLootData.creates[0], // Item
+                                                                0, // Reference
+                                                                100, // Chance
+                                                                0, // QuestRequired
+                                                                1, // LootMode
+                                                                0, // GroupId
+                                                                spellLootData.creates[1], // MinCount
+                                                                spellLootData.creates[2], // MaxCount
+                                                                ""); // Comment
+                }
+
+                returnSql += m_spellLootBuilder.ToString() + "\n";
+            }
+
+            return returnSql;
+        }
+        
+        protected SpellTemplateParsing m_spellTemplateData;
+        protected SpellLootParsing[] m_spellLootDatas;
+
+        protected SqlBuilder m_spellLootBuilder;
+    }
+}
---
+++ WowHeadParser/MainWindow.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WowHeadParser
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.comboBoxChoice = new System.Windows.Forms.ComboBox();
+            this.textBoxDe = new System.Windows.Forms.TextBox();
+            this.labelDe = new System.Windows.Forms.Label();
+            this.labelA = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBoxLocale = new System.Windows.Forms.ComboBox();
+            this.ProgressBarValue = new System.Windows.Forms.Label();
+            this.timeLeftLabel = new System.Windows.Forms.Label();
+            this.comboBoxEntity = new System.Windows.Forms.ComboBox();
+            this.selectList = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.progressBar1.Location = new System.Drawing.Point(18, 56);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(702, 35);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(631, 15);
+            this.textBoxA.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(88, 26);
+            this.textBoxA.TabIndex = 4;
+            this.textBoxA.Text = "0";
+            // 
+            // comboBoxChoice
+            // 
+            this.comboBoxChoice.FormattingEnabled = true;
+            this.comboBoxChoice.Location = new System.Drawing.Point(18, 15);
+            this.comboBoxChoice.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBoxChoice.Name = "comboBoxChoice";
+            this.comboBoxChoice.Size = new System.Drawing.Size(188, 28);
+            this.comboBoxChoice.TabIndex = 1;
+            this.comboBoxChoice.SelectedIndexChanged += new System.EventHandler(this.comboBoxChoice_SelectedIndexChanged);
+            this.comboBoxChoice.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBoxChoice_MouseClick);
+            // 
+            // textBoxDe
+            // 
+            this.textBoxDe.Location = new System.Drawing.Point(479, 15);
+            this.textBoxDe.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxDe.Name = "textBoxDe";
+            this.textBoxDe.Size = new System.Drawing.Size(88, 26);
+            this.textBoxDe.TabIndex = 3;
+            this.textBoxDe.Text = "1";
+            // 
+            // labelDe
+            // 
+            this.labelDe.AutoSize = true;
+            this.labelDe.Location = new System.Drawing.Point(418, 19);
+            this.labelDe.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDe.Name = "labelDe";
+            this.labelDe.Size = new System.Drawing.Size(52, 20);
+            this.labelDe.TabIndex = 5;
+            this.labelDe.Text = "Start :";
+            // 
+            // labelA
+            // 
+            this.labelA.AutoSize = true;
+            this.labelA.Location = new System.Drawing.Point(577, 19);
+            this.labelA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelA.Name = "labelA";
+            this.labelA.Size = new System.Drawing.Size(46, 20);
+            this.labelA.TabIndex = 6;
+            this.labelA.Text = "End :";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(558, 262);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 48);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start parsing";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBoxLocale
+            // 
+            this.comboBoxLocale.FormattingEnabled = true;
+            this.comboBoxLocale.Location = new System.Drawing.Point(126, 274);
+            this.comboBoxLocale.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBoxLocale.Name = "comboBoxLocale";
+            this.comboBoxLocale.Size = new System.Drawing.Size(118, 28);
+            this.comboBoxLocale.TabIndex = 6;
+            // 
+            // ProgressBarValue
+            // 
+            this.ProgressBarValue.AccessibleRole = System.Windows.Forms.AccessibleRole.ProgressBar;
+            this.ProgressBarValue.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ProgressBarValue.BackColor = System.Drawing.Color.Transparent;
+            this.ProgressBarValue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ProgressBarValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ProgressBarValue.Location = new System.Drawing.Point(18, 70);
+            this.ProgressBarValue.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ProgressBarValue.Name = "ProgressBarValue";
+            this.ProgressBarValue.Size = new System.Drawing.Size(702, 35);
+            this.ProgressBarValue.TabIndex = 1;
+            this.ProgressBarValue.Text = "0%";
+            this.ProgressBarValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timeLeftLabel
+            // 
+            this.timeLeftLabel.AutoSize = true;
+            this.timeLeftLabel.Location = new System.Drawing.Point(377, 276);
+            this.timeLeftLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.timeLeftLabel.Name = "timeLeftLabel";
+            this.timeLeftLabel.Size = new System.Drawing.Size(93, 20);
+            this.timeLeftLabel.TabIndex = 9;
+            this.timeLeftLabel.Text = "00h00m00s";
+            // 
+            // comboBoxEntity
+            // 
+            this.comboBoxEntity.FormattingEnabled = true;
+            this.comboBoxEntity.Location = new System.Drawing.Point(216, 15);
+            this.comboBoxEntity.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBoxEntity.Name = "comboBoxEntity";
+            this.comboBoxEntity.Size = new System.Drawing.Size(188, 28);
+            this.comboBoxEntity.TabIndex = 2;
+            this.comboBoxEntity.SelectedIndexChanged += new System.EventHandler(this.comboBoxEntity_SelectedIndexChanged);
+            this.comboBoxEntity.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBoxEntity_MouseClick);
+            // 
+            // selectList
+            // 
+            this.selectList.CheckBoxes = true;
+            this.selectList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.selectList.HideSelection = false;
+            this.selectList.Location = new System.Drawing.Point(18, 101);
+            this.selectList.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.selectList.Name = "selectList";
+            this.selectList.Size = new System.Drawing.Size(702, 146);
+            this.selectList.TabIndex = 5;
+            this.selectList.UseCompatibleStateImageBehavior = false;
+            this.selectList.View = System.Windows.Forms.View.List;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(280, 276);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Time left :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 277);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Language :";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 319);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.selectList);
+            this.Controls.Add(this.comboBoxEntity);
+            this.Controls.Add(this.timeLeftLabel);
+            this.Controls.Add(this.ProgressBarValue);
+            this.Controls.Add(this.comboBoxLocale);
+            this.Controls.Add(this.labelA);
+            this.Controls.Add(this.labelDe);
+            this.Controls.Add(this.textBoxDe);
+            this.Controls.Add(this.comboBoxChoice);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "Wowhead Parser";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.ComboBox comboBoxChoice;
+        private System.Windows.Forms.TextBox textBoxDe;
+        private System.Windows.Forms.Label labelDe;
+        private System.Windows.Forms.Label labelA;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBoxLocale;
+        private System.Windows.Forms.Label ProgressBarValue;
+        private System.Windows.Forms.Label timeLeftLabel;
+        private System.Windows.Forms.ComboBox comboBoxEntity;
+        private System.Windows.Forms.ListView selectList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
